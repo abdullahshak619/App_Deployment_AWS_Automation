12115bce74ce3bd521743ae8bc934307cc8871a4
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-05 16:51:05 +0530

    Update main.tf

--- Terraform/VPC/main.tf
+++ Terraform/VPC/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucketlko"
+    bucket         = "terraform-state-bucketlko"
     key            = "env/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
