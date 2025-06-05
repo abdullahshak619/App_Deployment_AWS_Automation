021e96d9430a3ab5ea8474033e83d2ecf2ffebcc
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-05 18:31:37 +0530

    Update main.tf

--- Terraform/VPC/main.tf
+++ Terraform/VPC/main.tf
@@ -100,6 +100,13 @@ resource "aws_security_group" "ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
 
   egress {
     from_port   = 0
