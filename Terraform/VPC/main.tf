2643f040e38435906f31b13f601e6c60a3126a78
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-05 15:34:58 +0530

    Update main.tf

--- Terraform/VPC/main.tf
+++ Terraform/VPC/main.tf
@@ -155,4 +155,13 @@ output "ec2_public_ip" {
   value = aws_instance.example.public_ip
 }
 
+##DONT RUN BELOW ONE IN CI/CD ,only for local
+
+# Output private key (secure this)
+#output "private_key_pem" {
+#  value     = tls_private_key.example.private_key_pem
+#  sensitive = true
+#}
+
+
 
