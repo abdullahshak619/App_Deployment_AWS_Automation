741c2de26d1e68e52f40447db163677b507f1ad4
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-05 16:39:24 +0530

    Create main.tf

---
+++ Terraform/s2/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "my-terraform-state-bucket"
+}
+
+#resource "aws_dynamodb_table" "terraform_locks" {
+#  name         = "terraform-locks"
+#  billing_mode = "PAY_PER_REQUEST"
+#  hash_key     = "LockID"
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
+#}
