6fd2df45ae489a3e13d66012bc537cf2f9f60e49
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-05 17:00:15 +0530

    Update main.tf

--- Terraform/VPC/main.tf
+++ Terraform/VPC/main.tf
@@ -11,6 +11,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-aut-bucket-name-123456"  # Make sure it's globally unique!
+  force_destroy = true  # Optional: allows deletion even if not empty
+}
+
 # Generate RSA Key Pair
 resource "tls_private_key" "example" {
   algorithm = "RSA"
