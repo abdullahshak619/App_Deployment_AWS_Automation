e054be04e4659b5cb2d02cdfe17d7a9d0d505b78
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-06 18:40:10 +0530

    Update main.tf

--- Terraform/ecs/main.tf
+++ Terraform/ecs/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucketlko"
+    key            = "env/dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
