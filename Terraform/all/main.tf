1f0b879c000859a1b94b67d09746dcc5c536099b
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-09 18:37:27 +0530

    Update main.tf

--- Terraform/all/main.tf
+++ Terraform/all/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-aut-bu3cket-name-12456"
+    bucket         = "terraform-state-bucketlko"
     key            = "env/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
@@ -14,7 +14,7 @@ provider "aws" {
 ###########################
 # S3 BUCKET FOR TERRAFORM STATE
 ###########################
-resource "aws_s3_bucket" "tf_state" {
+resource "aws_s3_bucket" "example" {
   bucket        = "my-aut-bu3cket-name-12456" # Must be globally unique
   force_destroy = true
 
@@ -242,6 +242,4 @@ output "ec2_public_ip" {
   value = aws_instance.example.public_ip
 }
 
-output "private_key_secret_arn" {
-  value = aws_secretsmanager_secret.ec2_private_key.arn
-}
+
