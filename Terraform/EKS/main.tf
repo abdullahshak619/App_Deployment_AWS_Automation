84734e1d28311b6bb77c32c7af20ecab5ad44416
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-14 16:12:23 +0530

    Update main.tf

--- Terraform/EKS/main.tf
+++ Terraform/EKS/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucketlko"
-    key            = "EKS/dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
 
 terraform {
   backend "s3" {
