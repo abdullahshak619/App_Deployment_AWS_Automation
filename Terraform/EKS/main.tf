2d6774471755b8f18d80bd0677a0a478aa400a2f
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-14 15:02:50 +0530

    Update main.tf

--- Terraform/EKS/main.tf
+++ Terraform/EKS/main.tf
@@ -27,6 +27,7 @@ resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
   tags = { Name = "eks-subnet-1" }
 }
 
@@ -34,6 +35,7 @@ resource "aws_subnet" "subnet2" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
   tags = { Name = "eks-subnet-2" }
 }
 
