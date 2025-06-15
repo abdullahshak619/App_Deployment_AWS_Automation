5d4819898604728d390289b20f21b32b8f4ea23b
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-15 16:44:17 +0530

    Update main.tf

--- Terraform/EKS/main.tf
+++ Terraform/EKS/main.tf
@@ -171,9 +171,9 @@ resource "aws_eks_cluster" "eks" {
     )
   }
 
-# access_config {
-#    authentication_mode = "API_AND_CONFIG_MAP"
-#  }
+ access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
