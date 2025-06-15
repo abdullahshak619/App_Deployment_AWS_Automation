fe53058207119745989acce9c6028b91b1f68296
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-15 14:54:34 +0530

    Update toaccessec2_eks.tf

--- Terraform/EKS/toaccessec2_eks.tf
+++ Terraform/EKS/toaccessec2_eks.tf
@@ -40,9 +40,11 @@ resource "aws_iam_role" "eks_access_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_access_policy_attach" {
+
+
+resource "aws_iam_role_policy_attachment" "eks_full_access" {
   role       = aws_iam_role.eks_access_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFullAccess"
 }
 
 # Optional: Allow reading secrets
